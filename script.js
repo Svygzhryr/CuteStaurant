b14b9cc8596134551be6654637d2ce73031b088b
Svygzhryr <pamixy@gmail.com>
2022-09-27 19:32:48 +0300

    lmao did stuff

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello biach');
+function initials(name) {
+    let arr = name.split('');
+    let res = [];
+    arr.forEach(element => {
+        if (element === element.toUpperCase()) {
+            res.push(element);
+        }
+    });
+    res = res.join('');
+    let regex = /\s/gi;
+    console.log(typeof res)
+    res = res.replace(regex, '.');
+    return res
+}
+
+console.log(initials('Hate Niggers'))
